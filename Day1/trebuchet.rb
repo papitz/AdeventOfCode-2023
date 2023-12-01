83c59aea2496696d4a2beb2eebbe39be02f1f758
papitz <paul@familie-hoeppner.de>
2023-12-01 12:17:20 -0800

    Finished part two of day 1

--- Day1/trebuchet.rb
+++ Day1/trebuchet.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
-DIGIT_EXPRESSION = /\D*(\d).*?(\d)?\D*$/.freeze
+WRITTEN_NUMBERS = {
+  'one' => '1',
+  'two' => '2',
+  'three' => '3',
+  'four' => '4',
+  'five' => '5',
+  'six' => '6',
+  'seven' => '7',
+  'eight' => '8',
+  'nine' => '9'
+}.freeze
+DIGIT_EXPRESSION = "(?=(#{WRITTEN_NUMBERS.keys.join('|')}|\\d))".freeze
+
 sum = 0
 File.readlines('input.txt', chomp: true).each do |line|
-  match = line.match(DIGIT_EXPRESSION)
-  number = match[1] + (match[2].nil? ? match[1] : match[2])
+  matches = line.scan(Regexp.new(DIGIT_EXPRESSION)).flatten
+
+  first = matches.first
+  last = matches.last
+  first = first.size > 1 ? WRITTEN_NUMBERS[first] : first
+  last = last.size > 1 ? WRITTEN_NUMBERS[last] : last
+  number = first + last
   sum += number.to_i
 end
 

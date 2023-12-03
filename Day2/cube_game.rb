4535d55f0dc4742bfca639d64d4aa3fdb7763095
papitz <paul@familie-hoeppner.de>
2023-12-02 20:14:54 -0800

    Finished part two of day 2 and cleaned up the code a bit

--- Day2/cube_game.rb
+++ Day2/cube_game.rb
@@ -1,25 +1,19 @@
 # frozen_string_literal: true
 
-max_cubes = {
-  'red' => 12,
-  'green' => 13,
-  'blue' => 14
-}
-GAME_SPLIT_EXPRESSION = /Game (\d+): (.*)/
+GAME_SPLIT_EXPRESSION = /Game \d+: (.*)/
 NUMBER_COLOR_EXPRESSION = /(\d+) (.*)/
 
 sum = File.readlines('input.txt', chomp: true).map do |line|
+  cubes = {}
   m = line.match(GAME_SPLIT_EXPRESSION)
-  game_number = m[1]
-  game_data = m[2].split(';')
+  game_data = m[1].split(';')
   game_data.each do |game|
     game.split(',').each do |cube|
       number_color = cube.match(NUMBER_COLOR_EXPRESSION)
-      game_number = 0 if number_color[1].to_i > max_cubes[number_color[2]]
+      cubes[number_color[2]] = [number_color[1].to_i, cubes[number_color[2]].to_i].max
     end
   end
-
-  game_number.to_i
+  cubes.values.inject(:*)
 end.sum
 
 puts "Sum: #{sum}"
